5bb42bf2b2c7222193c0bec7ecf4a7caa3feb4bd
KamiyamaShun <76637787+KamiyamaShun@users.noreply.github.com>
2021-01-25 11:12:53 +0900

    remove uniquness

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,7 +1,7 @@
 class Employee < ApplicationRecord
   with_options presence: true do
     validates :name
-    validates :employee_number,format: {with: /\A[0-9]+\z/, message: 'を半角数字で入力してください'}, uniqueness: true
+    validates :employee_number,format: {with: /\A[0-9]+\z/, message: 'を半角数字で入力してください'}
   end
 
   belongs_to :user
